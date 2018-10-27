8e3fd3de485b1d9a391e584a30e8c3911561031e
gayathribuddhika <39160904+gayathribuddhika@users.noreply.github.com>
2018-10-27 15:59:09 +0530

    forth program
    if else

---
+++ IfElse.java
@@ -0,0 +1,14 @@
+
+public class IfElse {
+
+    
+    public static void main(String[] args) {
+        int marks=68;
+        if(marks>50){
+            System.out.println("you are pass");
+        }else{
+            System.out.println("you are fail");
+        }
+    }
+    
+}
